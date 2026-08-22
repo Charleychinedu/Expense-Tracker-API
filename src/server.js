56aaf9108774b8a5cbcb422654cc82de928a6350
Charley <ezecharles706@gmail.com>
2026-08-22 11:15:29 +0100

    Added /GET route for testing on deployment

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const app = require("./app");
 
-
-const PORT = process.env.PORT || 8888
-
-
+const PORT = process.env.PORT || 8888;
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Expense Tracker API is running" });
+});
